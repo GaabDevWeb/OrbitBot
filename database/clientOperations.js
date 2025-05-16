3705332fc86dca74346bb0214ed0c9e3b5d10374
Gaab <154674155+GaabDevWeb@users.noreply.github.com>
2025-05-16 19:40:26 -0300

    Update clientOperations.js

--- database/clientOperations.js
+++ database/clientOperations.js
@@ -1,1 +1,13 @@
 const { readClientes, writeClientes } = require('./dbOperations');
+
+const cadastrarCliente = (numero) => {
+    const clientes = readClientes();
+    const novoCliente = {
+        id: clientes.length + 1,
+        numero,
+        created_at: new Date().toISOString()
+    };
+    clientes.push(novoCliente);
+    writeClientes(clientes);
+    return novoCliente;
+};
