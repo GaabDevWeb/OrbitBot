a22c129a596c4fd9d8f2cc2946d6464d532d7534
Gaab <154674155+GaabDevWeb@users.noreply.github.com>
2025-05-16 19:41:40 -0300

    Update dbOperations.js

--- database/dbOperations.js
+++ database/dbOperations.js
@@ -1,2 +1,5 @@
 const fs = require('fs');
 const path = require('path');
+
+const clientesPath = path.join(__dirname, 'data/clientes.json');
+const historicoPath = path.join(__dirname, 'data/historico.json');
